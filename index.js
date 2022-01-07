8a04c73fc94f6ebd6eb0fe6f4492b6d40cb0b8bb
tententgc <nyman3527@gmail.com>
2022-01-07 16:22:54 +0700

    s

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
-const {Client , Intents} = require('discord.js'); // Import discord.js 
-const dotenv = require('dotenv'); // Import dotenv
-dotenv.config(); // Load .env file 
+const { Client, Intents, MessageActionRow } = require('discord.js'); // Import discord.js 
+const config = require('./config.json');
 
-const client = new Client({ 
-    intents:[
+
+const client = new Client({
+    intents: [
         Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,   
+        Intents.FLAGS.GUILD_MESSAGES,
     ]
 });
 
-client.on('ready', () => {
+const random_num = () => {
+    return Math.floor(Math.random() * 5);
+}
+
+client.once('ready', () => {
     console.log("CareSasi is ready!");  // When the bot is ready, print "CareSasi is ready!"
-}); 
+});
 
 client.on('messageCreate', msg => {
-    console.log(msg)
+
+    if (!msg.content.startsWith(config.prefix)) return; // If the message doesn't start with the prefix, return.
+
+    const args = msg.content.substring(config.prefix.length).split(/ +/);
+
+    switch (args[0]) {
+        case "hello":
+            msg.reply("Hello!"); 
+
+            break;
+
+        case "say":
+            msg.reply(args.slice(1).join(" ")); // Reply with the message after the command.
+
+            break;
+    }
+
 });
-client.login(process.env.TOKEN); // Login to discord 
+
+client.login(config.token); // Login to discord 
