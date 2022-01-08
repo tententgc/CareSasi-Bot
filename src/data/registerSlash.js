534465f9b58c305ff33e59cce5e9ea5ee2d921c8
tententgc <nyman3527@gmail.com>
2022-01-08 14:15:50 +0700

    Update registerSlash.js

--- src/data/registerSlash.js
+++ src/data/registerSlash.js
@@ -1,9 +1,39 @@
 const {Client , ApplicationCommandData ,Snowflake} = require("discord.js"); 
+
+const handleErr = (err) => {
+    if (err.name !== "DiscordAPIError: Missing Access") {
+        console.log("[⨯] Failed to Register Slash Command,", red, err.stack);
+    }
+};
 /**
  * 
  * @param {Client} client 
- * @param {ApplicationCommandData[]} slashCommandsArr 
+ * @param {?ApplicationCommandData[]} slashCommandsArr 
  * @param {Snowflake} guildId 
- * @param {*} command 
+ * @param {String} command 
  */
-module.exports =(client, slashCommandsArr , guildId ,command ) => {};
+module.exports =(client, slashCommandsArr , guildId ,command ) => {
+    try{ 
+        let len =  slashCommandsArr?.length; 
+
+        //Register Slash Command in specific Guild 
+        if (guildId){
+            const guild = await client.guilds.fetch(guildId); 
+            if (!guild){ 
+                return console.log("[⨯] Invalid Guild ID".red);
+            }
+
+            //Register specific Command in specific Guild 
+            if (command) {
+                const cmd = slashCommandsArr.find((e) => e.name === command);
+                if (!cmd) {
+                    return console.log("[×] {".red + command.cyan + "} Command Not Found.".red);
+                }
+
+                await guild.commands.create(cmd).catch(handleErr); 
+        }
+    }
+    }catch{
+
+    }
+};
