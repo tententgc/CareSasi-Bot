9a533aaecaeb468cdd34431a76b16bb706e7eb77
tententgc <nyman3527@gmail.com>
2022-01-09 15:16:57 +0700

    Update help.js

--- src/commands/Info/help.js
+++ src/commands/Info/help.js
@@ -1,9 +1,9 @@
 const { Message, Client, MessageEmbed } = require("discord.js");
-const fs = require("fs"); 
+const fs = require("fs");
 const prefix = require("../../data/config.json").prefix;
 module.exports = {
     name: "help",
-    aliases: ["h"], 
+    aliases: ["h"],
     description: "help with all command Info",
 
     /**
@@ -13,34 +13,50 @@ module.exports = {
      * @param {String[]} args
      */
     run: async (client, message, args) => {
-        if (!args[0]){ 
-            let categories = []; 
+        if (!args[0]) {
+            let categories = [];
 
-            fs.readdirSync("./commands/").forEach(dir => { 
-                const commands = fs.readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js")); 
-            
+            fs.readdirSync("./commands/").forEach(dir => {
+                const commands = fs.readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
+
                 const cmds = commands.map(command => {
-                    let file = require(`../../commands/${dir}/${command}`); 
+                    let file = require(`../../commands/${dir}/${command}`);
 
-                    if (!file.name) return 'No command name'; 
+                    if (!file.name) return 'No command name';
 
-                    let name  = file.name.replace(".js", ""); 
+                    let name = file.name.replace(".js", "");
 
                     return `\`${name}\``;
                 });
 
-                let data = new Object(); 
+                let data = new Object();
 
-                data  = { 
-                    name : dir.toUpperCase(), 
-                    value : cmds.length  === 0? 'In progress' : cmds.join(", ") 
+                data = {
+                    name: dir.toUpperCase(),
+                    value: cmds.length === 0 ? 'In progress' : cmds.join(", ")
                 }
 
-                categories.push(data); 
-                
-
+                categories.push(data);
 
             });
+
+            //edit embed
+            const helpEmbed = new MessageEmbed()
+            .setTitle('Help Menu')
+            .addFields(categories)
+            .setDescription(`Use \`${prefix}help <command>\` to get more information about a command.`)
+            .setFooter(client.user.username, client.user.displayAvatarURL({ dynamic: true })) 
+            .setColor("RANDOM")
+            .setTimestamp(); 
+        }else{ 
+            const command  = client.commands.get(args[0]).toLowerCase() || client.commands.find(c => c.aliases && c.aliases.includes(args[0].toLowerCase()));
+        
+            if (!command){
+                 const noCommandEmbed = new MessageEmbed() 
+                .setTitle(`Command not found Use \`${prefix}help\` to see all commands.`)
+                .setColor("RANDOM")
+                .setFooter(client.user.username, client.user.displayAvatarURL({ dynamic: true }))
+            }
         }
     },
 };
