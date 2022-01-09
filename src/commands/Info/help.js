9768e63aff004f00e3006eaf978831e32cd37ae5
tententgc <nyman3527@gmail.com>
2022-01-09 15:51:06 +0700

    save  help fot debug

--- src/commands/Info/help.js
+++ src/commands/Info/help.js
@@ -48,15 +48,33 @@ module.exports = {
             .setFooter(client.user.username, client.user.displayAvatarURL({ dynamic: true })) 
             .setColor("RANDOM")
             .setTimestamp(); 
+
+            return message.channel.send({ embed: [helpEmbed] });
         }else{ 
             const command  = client.commands.get(args[0]).toLowerCase() || client.commands.find(c => c.aliases && c.aliases.includes(args[0].toLowerCase()));
         
             if (!command){
                  const noCommandEmbed = new MessageEmbed() 
-                .setTitle(`Command not found Use \`${prefix}help\` to see all commands.`)
+                .setTitle(`Command not found`)
+                .setDescription(`Use \`${prefix}help <command>\` to get more information about a command.`)
                 .setColor("RANDOM")
                 .setFooter(client.user.username, client.user.displayAvatarURL({ dynamic: true }))
+                .setTimestamp();
+
+                return message.channel.send({ embed: [noCommandEmbed] });
             }
+            const helpMenuEmbed = new MessageEmbed()
+            .setTitle(`Command Inforamton`)
+            .addFields(`Prefix`,`\`${prefix}\``)
+            .addFields(`Command`, command.name ? `\`${command.name}\`` : 'No command name')
+            .addFields(`Aliases`, command.aliases ? `\`${command.aliases.join('` `')}\`` : 'No aliases')
+            .addFields(`Description`, command.description ? command.description : 'No description')
+            .addFields(`Usage`, command.usage ? `\`${prefix}${command.name} ${command.usage}\`` : 'No usage')
+            .setFooter(client.user.username, client.user.displayAvatarURL({ dynamic: true }))
+            .setColor("RANDOM")
+            .setTimestamp();
+            console.log(helpMenuEmbed);
+            return message.channel.send({ embeds: [helpMenuEmbed] }); 
         }
     },
 };
